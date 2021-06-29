549b4336843c175f765e8fdfb35b592cbf360ecc
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-06-29 09:56:49 +0530

    Classes Basics added

---
+++ Basics/Classes/Recepitionist.java
@@ -0,0 +1,13 @@
+public class Recepitionist {
+    private int staffID;
+    private String name;
+    private String telephoneNumber;
+
+    public Boolean checkRoomAvailability(int roomNumber){
+        return true;
+    }
+    public double generateBill(){
+        return 0;
+    }
+    public void takeCustomerFeddback() { }
+}
