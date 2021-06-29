cb35766f071675b950a7265a6e31921a098c934c
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-06-29 09:55:59 +0530

    Printing Basics added

---
+++ Basics/Printing.java
@@ -0,0 +1,20 @@
+public class Printing {
+    public static void main(String[] args) {
+        // Print without newline
+        System.out.print("Thushara");
+
+        // Print with newline
+        System.out.println("Thushara");
+        System.out.print("Thiwanka\n");
+        
+        // Print variable
+        int num1 = 10;
+        System.out.println(num1);
+        
+        // Print variable with string
+        int num2 = 20;
+        System.out.println("Number = " + num2);
+
+        System.out.println("Number1 = " + num1 + "\n" + "Number2 = " + num2);
+    }
+}
