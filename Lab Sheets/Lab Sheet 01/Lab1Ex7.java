093d4f883c411745c04ceec79f20e783646c3f2d
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-07-14 14:59:54 +0530

    Lab 01 Ex 07 Added

---
+++ Lab Sheets/Lab Sheet 01/Lab1Ex7.java
@@ -0,0 +1,30 @@
+/*  Write a Java program to add two integers and display the sum and the average of two Integers.
+    Declare two integer variables to store the numbers to be added.
+*/
+
+public class Lab1Ex7 {
+    public static void main(String[] args) {
+        
+        // Declare two integer variables to store the numbers to be added or find average.
+        int num1 = 4, num2 = 6;
+
+        // Call the method to add two integers
+        System.out.println("Sum = " + sum(num1, num2));
+        
+        // Call the method to find average of two integers
+        System.out.println("Average = " + average(num1, num2));
+
+    }
+
+    // Method to add two integers
+    private static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    // Method to find the average of two integers
+    private static int average(int num1, int num2) {
+        return sum(num1, num2) / 2;
+    }
+}
+
+// </Thushara Thiwanka>
