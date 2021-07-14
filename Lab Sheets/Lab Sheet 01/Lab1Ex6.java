69e7e6b2ba87cd88732ffd0902c24501edf80bab
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-07-14 14:59:42 +0530

    Lab 01 Ex 06 Added

---
+++ Lab Sheets/Lab Sheet 01/Lab1Ex6.java
@@ -0,0 +1,37 @@
+/* Write a program that draws the following figures one next the other.
+    
+    * * * * *       *
+    * * * * *      * *
+    * * * * *     * * *
+    * * * * *    * * * *
+    * * * * *   * * * * *
+
+*/
+
+public class Lab1Ex6 {
+    public static void main(String[] args) {
+
+        int column = 0;
+        int raw = 0;
+
+        for(column = 1; column <= 5; column++) {
+            for(raw = 0; raw < 5; raw++) {
+                System.out.print("* ");
+            }
+
+            System.out.print("\t");
+
+            for(raw = 0; raw < 5 - column; raw ++) {
+                System.out.print(" ");
+            }
+
+            for(raw = 0; raw < column; raw ++) {
+                System.out.print("* ");
+            }
+
+            System.out.print("\n");
+        }
+    }
+}
+
+// </Thushara Thiwanka>
