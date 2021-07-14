70fbc0cd971a5ea15f452e9073d7f4e5aa4a09aa
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-07-14 14:59:12 +0530

    Lab 01 Ex 03 Added

---
+++ Lab Sheets/Lab Sheet 01/Lab1Ex3.java
@@ -0,0 +1,18 @@
+/*  Write a program to validate a given age of a person using if-else statements.
+    If age is greater than 18 it should print “Adult” otherwise print “Child”.
+*/
+
+public class Lab1Ex3 {
+    public static void main(String[] args) {
+        int age = 19;
+
+        if(age > 18) {
+            System.out.println("Adult");
+        }
+        else {
+            System.out.println("Child");
+        }
+    }
+}
+
+// </Thushara Thiwanka>
