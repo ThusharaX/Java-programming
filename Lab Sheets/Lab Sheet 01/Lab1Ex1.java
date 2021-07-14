f44b8bcc7b59bfc1712e443fe2c4dff7dfd71b05
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-07-14 14:58:46 +0530

    Lab 01 Ex 01 Added

---
+++ Lab Sheets/Lab Sheet 01/Lab1Ex1.java
@@ -0,0 +1,14 @@
+/* Write a program that produces the following output:
+
+    Hello World!
+    It's been nice knowing you.
+    Goodbye world!
+*/
+
+public class Lab1Ex1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!\nIt's been nice knowing you.\nGoodbye world!");
+    }
+}
+
+// </Thushara Thiwanka>
