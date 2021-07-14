13a5ad6676a7038ae887a335cff839dea0e484fb
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-07-14 14:58:59 +0530

    Lab 01 Ex 02 Added

---
+++ Lab Sheets/Lab Sheet 01/Lab1Ex2.java
@@ -0,0 +1,23 @@
+/* Write a program to get the following output:
+    ===================================
+    =       Student Information       =
+    ===================================
+    = Name          : ARDP Ranasignhe =
+    = Reg No        : DIS\08\M4\1234  =
+    = Address       : Malabe          =
+    ===================================
+*/
+
+public class Lab1Ex2 {
+    public static void main(String[] args) {
+        System.out.println("=========================================");
+        System.out.println("=\tStudent Information\t\t=");
+        System.out.println("=========================================");
+        System.out.println("= Name\t\t: ARDP Ranasignhe\t=");
+        System.out.println("= Reg No\t: DIS\\08\\M4\\1234\t=");
+        System.out.println("= Address\t: Malabe\t\t=");
+        System.out.println("=========================================");
+    }
+}
+
+// </Thushara Thiwanka>
