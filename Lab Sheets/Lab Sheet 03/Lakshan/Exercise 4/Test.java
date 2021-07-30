b45a3d481e295c0f8baeca5aa0f2759a9e970108
Pasindu Lakshan <74607482+pasindulakshan@users.noreply.github.com>
2021-07-30 18:13:14 +0530

    Add Exercise 4

---
+++ Lab Sheets/Lab Sheet 03/Lakshan/Exercise 4/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student("Pasindu","IT20123456","Galle");
+
+        System.out.println(s1.getDetails());
+    }
+}
