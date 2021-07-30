1bbcbea56c55c967a443063478c2bbf27f8ffc09
Pasindu Lakshan <74607482+pasindulakshan@users.noreply.github.com>
2021-07-30 18:23:45 +0530

    Add Comments to Lab sheet 3 Exercises

--- Lab Sheets/Lab Sheet 03/Lakshan/Exercise 1/Test.java
+++ Lab Sheets/Lab Sheet 03/Lakshan/Exercise 1/Test.java
@@ -1,8 +1,10 @@
 public class Test {
     public static void main(String[] args) {
+        // Create Students Objects
         Student s1 = new Student();
         Student s2 = new Student();
 
+        // Assign values to the objects
         s1.name = "Pasindu";
         s1.ditno = "IT20123456";
         s1.address = "Galle";
@@ -10,6 +12,8 @@ public class Test {
         s2.name = "Lakshan";
         s2.ditno = "IT20123457";
         s2.address = "Matara";
+
+        // Display the values
         System.out.println("Student 1");
         System.out.println("Name: " + s1.name + "\nDitno: " + s1.ditno + "\nAddress: " + s1.address + "\n");
         System.out.println("Student 2");
