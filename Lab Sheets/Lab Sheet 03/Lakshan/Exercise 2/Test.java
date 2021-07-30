40d096865af93f53f6fa995c3cb076a91740e968
Pasindu Lakshan <74607482+pasindulakshan@users.noreply.github.com>
2021-07-30 18:12:54 +0530

    Add Exercise 2

---
+++ Lab Sheets/Lab Sheet 03/Lakshan/Exercise 2/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student("Pasindu","IT20123456","Galle");
+        Student s2 = new Student("Lakshan","IT20123457","Matara");
+    }
+}
