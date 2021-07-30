047b99ace87ea0e7adfbb8e940c24cb59355fe86
Pasindu Lakshan <74607482+pasindulakshan@users.noreply.github.com>
2021-07-30 19:11:33 +0530

    Add Exercise 8

---
+++ Lab Sheets/Lab Sheet 03/Lakshan/Exercise 8/Cat.java
@@ -0,0 +1,7 @@
+public class Cat extends Pet{ 
+    private int livesLeft; 
+    public Cat(String n, String o, int a,  int l) { 
+        super(n, o, a); 
+        this.livesLeft = l; 
+    } 
+}//end of the pet class 
---
+++ Lab Sheets/Lab Sheet 03/Lakshan/Exercise 8/Main.java
@@ -0,0 +1,8 @@
+public class Main {  
+    public static void main(String[] args){  
+        Pet p = new Pet("Lissie","Smith",3);  
+        p.showDetails();  
+        Cat c = new Cat("Kyan", "Silva", 4, 4);  
+        c.showDetails();  
+    }  
+}//end of the demo class
