e2caa34ca0130b3b4f6d8af353e071766a8f6108
ShehaniHettiarachchi <88665593+ShehaniHettiarachchi@users.noreply.github.com>
2022-10-22 23:54:43 +0530

    Ticketing process using queue

---
+++ Basics/Data Structure/Queue-ticketingProcess/main.java
@@ -0,0 +1,15 @@
+package Task1;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Queue queue = new Queue(5);
+
+	    queue.insert(1);
+	    queue.insert(2);
+	    
+	    }
+
+}
